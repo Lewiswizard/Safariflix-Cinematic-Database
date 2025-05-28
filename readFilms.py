c6a84a63fa4f789321c5a0a99c0cfc63b6f1f38f
Lewis <lewisdicksonn@gmail.com>
2025-05-28 16:50:50 +0300

    readFilms

--- readFilms.py
+++ readFilms.py
@@ -1,1 +1,15 @@
 #readFilms
+from connect import *
+
+def read():
+	cursor.execute("SELECT * FROM film")
+	rows = cursor.fetchall()
+
+
+	print("\nFilm List:")
+	for record in rows:
+		
+		print(record)
+
+if __name__ == "__main__":
+	read()
