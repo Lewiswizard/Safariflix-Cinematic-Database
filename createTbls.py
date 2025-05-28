78d20a469cbced0ceac84f347368411ec7af882e
Lewis <lewisdicksonn@gmail.com>
2025-05-28 17:09:34 +0300

    functionality to create tables

--- createTbls.py
+++ createTbls.py
@@ -1,1 +1,17 @@
 #createTbls
+from connect import *
+
+cursor.execute(
+"""
+CREATE TABLE "film"(
+	"FilmID"	INTEGER NOT NULL UNIQUE,
+	"Title" TEXT,
+	"Year" INT,
+	"Rating" TEXT,
+	"Duration" INT,
+	"Genre" TEXT,
+	PRIMARY KEY("FilmID" AUTOINCREMENT)
+)
+"""
+)
+
